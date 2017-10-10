5e9a38b5dc33155d4cba826d39b4d60763745e98
Virgil Xu <virgilxu89@gmail.com>
2017-10-09 22:18:16 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+      loaders: [
+          {
+              test: /\.js$/,
+              loader: 'babel-loader',
+              query: {
+                  presets: ['react']
+              }
+          }
+      ]
+  }
+};
